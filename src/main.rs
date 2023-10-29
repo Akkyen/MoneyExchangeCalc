11139b4e9aa3491efa6362f6a61624125923055f
Benedikt Papiorek <benedikt.papiorek@informatik.hu-berlin.de>
2023-10-29 09:28:59 +0100

    Fixed Warnings and Added some Comments Updated crate name Removed unused imports Removed unused inits

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use std::io::{Read, stdin, Write};
+use std::io::{stdin};
 
 // Importing necessary libraries and modules
 use chrono::NaiveDate;
@@ -21,24 +21,25 @@ pub struct CurrencyJsonDataset {
 fn main() -> Result<()> {
 	// Main loop
 	loop {
-		let mut amount_cur_source = 1.0;
+		let mut amount_cur_source: f32;
 
-		let mut cur_code_source = String::new();
-		let mut cur_code_target = String::new();
+		let mut cur_code_source : String;
+		let mut cur_code_target : String;
 
 
+		// Gets a map of all supported currencies.
+		// The map goes cur_code -> cur_full_name
 		let currencies: HashMap<String, String> = serde_json::from_str(
 			&*reqwest::blocking::get("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json")
 				.unwrap().text().unwrap()).
-			expect("ERROR: Either there is no JSON at the used URL or the JSON could not get serialized to the internal representation!");
+			expect("ERROR: Either there is no JSON provided at the used URL or the JSON could \
+			 not get serialized to the internal representation!");
 
 
 		println!("With the input of \"exit\", the program can be terminated at any time.");
 
 
 		loop {
-			cur_code_source = String::new();
-
 			cur_code_source = utils::cli_read_line("Please enter the code of the currency you want to use: ").1;
 
 			utils::check_for_exit_prompt(&cur_code_source);
@@ -51,7 +52,7 @@ fn main() -> Result<()> {
 		}
 
 
-		//Getting the currency conversion modifiers for cur_code_source
+		// Getting the currency conversion modifiers for cur_code_source
 		let api_url = format!("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/{}.json", &cur_code_source);
 		let data = utils::currency_json_parsing(&*reqwest::blocking::get(api_url).unwrap().text().unwrap()).unwrap();
 
@@ -64,7 +65,7 @@ fn main() -> Result<()> {
 		loop {
 			let msg = format!("Please enter the amount of {} you want to convert: ", currencies.get(&*cur_code_source).unwrap());
 
-			let mut amount_as_str = utils::cli_read_line(&*msg).1;
+			let amount_as_str = utils::cli_read_line(&*msg).1;
 
 			println!();
 
@@ -89,7 +90,7 @@ fn main() -> Result<()> {
 		}
 
 
-		//Currency Code - Loop
+		// Currency Code - Loop
 		loop {
 			let msg = "Please enter the code of the currency you want to convert to: ";
 
@@ -118,6 +119,6 @@ fn main() -> Result<()> {
 
 		// Clearing the console screen
 		print!("{}[2J", 27 as char);
-		stdin().read_line(&mut "".to_string());
+		let _ = stdin().read_line(&mut "".to_string());
 	}
 }
